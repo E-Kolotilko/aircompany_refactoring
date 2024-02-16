79213e520bed01e6abbda06860727237e1cd1e48
John <e.kolotilko@gmail.com>
2024-02-16 21:46:49 +0300

    Added several tests

--- Java/src/test/java/AirportTest.java
+++ Java/src/test/java/AirportTest.java
@@ -3,24 +3,27 @@ import models.ClassificationLevel;
 import models.ExperimentalTypes;
 import models.MilitaryType;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import Planes.MilitaryPlane;
 import Planes.PassengerPlane;
 import Planes.Plane;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AirportTest {
     private static final PassengerPlane planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
 
+    private List<PassengerPlane> passengerPlanesSource;
+    private List<MilitaryPlane> militaryPlanesSource;
+    private List<ExperimentalPlane> experimentalPlanesSource;
     private List<Plane> planes;
 
     @BeforeTest
     private void createPlanes() {
-        planes = Arrays.asList(
+        passengerPlanesSource = Arrays.asList(
                 new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
                 new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
                 new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
@@ -28,16 +31,23 @@ public class AirportTest {
                 new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
                 new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
                 new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
-                new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
+                new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196)
+        );
+        militaryPlanesSource = Arrays.asList(
                 new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
                 new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
                 new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
                 new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
                 new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
-                new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT),
+                new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
+        );
+        experimentalPlanesSource = Arrays.asList(
                 new ExperimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
                 new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
         );
+        planes = new ArrayList<>(passengerPlanesSource);
+        planes.addAll(militaryPlanesSource);
+        planes.addAll(experimentalPlanesSource);
     }
 
     @Test
@@ -75,11 +85,37 @@ public class AirportTest {
     }
 
     @Test
-    public void testGetExperimentalPlanes() {
-        List<ExperimentalPlane> experimentalPlanes = new Airport(planes).getExperimentalPlanes();
+    public void testSortByMaxDistance() {
+        List<? extends Plane> planesSortedByMaxLoadCapacity = new Airport(planes).sortByMaxDistance().getPlanes();
 
-        Assert.assertEquals(experimentalPlanes.size(), 2);
-        Assert.assertTrue(experimentalPlanes.stream().anyMatch(plane -> plane.getModel().equals("Bell X-14")));
-        Assert.assertTrue(experimentalPlanes.stream().anyMatch(plane -> plane.getModel().equals("Ryan X-13 Vertijet")));
+        for (int i = 0; i < planesSortedByMaxLoadCapacity.size() - 1; i++) {
+            Assert.assertTrue(planesSortedByMaxLoadCapacity.get(i).getMaxFlightDistance()
+                    <= planesSortedByMaxLoadCapacity.get(i + 1).getMaxFlightDistance());
+        }
+    }
+
+    @Test
+    public void testSortByMaxSpeed() {
+        List<? extends Plane> planesSortedByMaxLoadCapacity = new Airport(planes).sortByMaxSpeed().getPlanes();
+
+        for (int i = 0; i < planesSortedByMaxLoadCapacity.size() - 1; i++) {
+            Assert.assertTrue(planesSortedByMaxLoadCapacity.get(i).getMaxSpeed()
+                    <= planesSortedByMaxLoadCapacity.get(i + 1).getMaxSpeed());
+        }
+    }
+
+    @Test
+    public void testGetExperimentalPlanes() {
+        Assert.assertEqualsNoOrder(new Airport(planes).getExperimentalPlanes().toArray(), experimentalPlanesSource.toArray());
+    }
+
+    @Test
+    public void testGetPassengerPlanes() {
+        Assert.assertEqualsNoOrder(new Airport(planes).getPassengerPlanes().toArray(), passengerPlanesSource.toArray());
+    }
+
+    @Test
+    public void testGetMilitaryPlanes() {
+        Assert.assertEqualsNoOrder(new Airport(planes).getMilitaryPlanes().toArray(), militaryPlanesSource.toArray());
     }
 }
